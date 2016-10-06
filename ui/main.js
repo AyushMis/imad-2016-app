853e5a461a65bf28c8d2be609dab1e24cebbdf53
AyushMis <mishraayush945@gmail.com>
2016-10-06 16:42:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
-console.log('Loaded!');
-
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
-var img = document.getElementById('pic');
-img.onclick = function(){
-    img.style.marginLeft = '100px';
+// counter code
+var button=document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    counter = counter + 1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 }
