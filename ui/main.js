d0ae9addcfa36546d3776bdbed81c9690d1b2742
AyushMis <mishraayush945@gmail.com>
2016-10-06 16:54:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,9 +5,15 @@ button.onclick = function(){
     
     var request= new XMLHttpRequest();
     request.onreadystatechange = function(){
-        if(request.readystate === XMLHttpRequest.DONE )
-    }
-    counter = counter + 1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+        if(request.readystate === XMLHttpRequest.DONE ){
+            if(request,status === 200){
+                var counter = request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.OPEN('GET','http://ayushmis.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
 }
